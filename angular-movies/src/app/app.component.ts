d73275e4c52e5dcd694519db2e281ac6ef801d02
VigilBushido <vigilbushido@gmail.com>
2025-01-10 14:43:08 -0500

    +poster

--- angular-movies/src/app/app.component.ts
+++ angular-movies/src/app/app.component.ts
@@ -19,22 +19,26 @@ export class AppComponent {
   movies = [{
     title: 'The Pursuit of Happyness',
     releaseDate: new Date(),
-    revenue: 307077295.00
+    revenue: 307077295.00,
+    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lBYOKAMcxIvuk9s9hMuecB9dPBV.jpg'
   },
   {
     title: 'Iron Man',
     releaseDate: new Date(),
-    revenue: 585174222.00
+    revenue: 585174222.00,
+    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/78lPtwv72eTNqFW9COBYI0dWDJa.jpg'
   },
   {
     title: 'Seven Pounds',
     releaseDate: new Date(),
-    revenue: 168167691.00
+    revenue: 168167691.00,
+    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/85zUipgVy2QbvAJ6djZXYaGckMv.jpg'
   },
   {
     title: 'A Beautiful Mind',
     releaseDate: new Date(),
-    revenue: 316800000.00
+    revenue: 316800000.00,
+    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/rEIg5yJdNOt9fmX4P8gU9LeNoTQ.jpg'
   }];
 
   duplicateNumber(value: number): number {
