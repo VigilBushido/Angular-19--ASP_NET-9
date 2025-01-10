1fb0e43d5227655d9eda1a59feeb7e993c379767
VigilBushido <vigilbushido@gmail.com>
2025-01-10 12:39:19 -0500

    @for & standalone import array

--- angular-movies/src/app/app.component.ts
+++ angular-movies/src/app/app.component.ts
@@ -1,12 +1,43 @@
+import { CurrencyPipe, DatePipe, UpperCasePipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, DatePipe, UpperCasePipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-movies';
+  single_movie = {
+    title: 'Castaway',
+    releaseDate: new Date(),
+    price: 1400.99
+  };
+
+  movies = [{
+    title: 'Pursuit of Happiness',
+    releaseDate: new Date(),
+    price: 2200.99
+  },
+  {
+    title: 'Iron Man',
+    releaseDate: new Date(),
+    price: 1700.99
+  },
+  {
+    title: 'Seven Pounds',
+    releaseDate: new Date(),
+    price: 2800.99
+  },
+  {
+    title: 'A Beautiful Mind',
+    releaseDate: new Date(),
+    price: 1999.99
+  }];
+
+  duplicateNumber(value: number): number {
+    return value * 2;
+  };
 }
