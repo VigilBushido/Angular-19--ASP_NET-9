7ef8d275270078b09fc4f2586c0a2ff8e46e44e6
VigilBushido <vigilbushido@gmail.com>
2025-01-11 07:43:20 -0500

    loading setTimeout

--- angular-movies/src/app/app.component.ts
+++ angular-movies/src/app/app.component.ts
@@ -16,30 +16,36 @@ export class AppComponent {
     revenue: 429632142.00
   };
 
-  movies = [{
-    title: 'The Pursuit of Happyness',
-    releaseDate: new Date(),
-    revenue: 307077295.00,
-    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lBYOKAMcxIvuk9s9hMuecB9dPBV.jpg'
-  },
-  {
-    title: 'Iron Man',
-    releaseDate: new Date(),
-    revenue: 585174222.00,
-    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/78lPtwv72eTNqFW9COBYI0dWDJa.jpg'
-  },
-  {
-    title: 'Seven Pounds',
-    releaseDate: new Date(),
-    revenue: 168167691.00,
-    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/85zUipgVy2QbvAJ6djZXYaGckMv.jpg'
-  },
-  {
-    title: 'A Beautiful Mind',
-    releaseDate: new Date(),
-    revenue: 316800000.00,
-    poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/rEIg5yJdNOt9fmX4P8gU9LeNoTQ.jpg'
-  }];
+  movies: any[] = [];
+
+  constructor() {
+    setTimeout(() => {
+      this.movies = [{
+        title: 'The Pursuit of Happyness',
+        releaseDate: new Date(),
+        revenue: 307077295.00,
+        poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/lBYOKAMcxIvuk9s9hMuecB9dPBV.jpg'
+      },
+      {
+        title: 'Iron Man',
+        releaseDate: new Date(),
+        revenue: 585174222.00,
+        poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/78lPtwv72eTNqFW9COBYI0dWDJa.jpg'
+      },
+      {
+        title: 'Seven Pounds',
+        releaseDate: new Date(),
+        revenue: 168167691.00,
+        poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/85zUipgVy2QbvAJ6djZXYaGckMv.jpg'
+      },
+      {
+        title: 'A Beautiful Mind',
+        releaseDate: new Date(),
+        revenue: 316800000.00,
+        poster: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/rEIg5yJdNOt9fmX4P8gU9LeNoTQ.jpg'
+      }];
+    }, 2000);
+  }
 
   duplicateNumber(value: number): number {
     return value * 2;
