798f849cd2b863ed33b478494aed05ec2dea4798
VigilBushido <vigilbushido@gmail.com>
2025-02-05 22:41:17 -0500

    + genres route

--- angular-movies/src/app/shared/components/menu/menu.component.ts
+++ angular-movies/src/app/shared/components/menu/menu.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 @Component({
   selector: 'app-menu',
-  imports: [NzButtonModule, NzIconModule, NzMenuModule],
+  imports: [NzButtonModule, NzIconModule, NzMenuModule, RouterLink],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
