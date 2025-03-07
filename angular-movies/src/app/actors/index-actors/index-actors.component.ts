c6558cd046ff9b9a54f4377b29c449c1de6570a0
VigilBushido <vigilbushido@gmail.com>
2025-03-06 23:50:36 -0500

    + created components

---
+++ angular-movies/src/app/actors/index-actors/index-actors.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-actors',
+  imports: [],
+  templateUrl: './index-actors.component.html',
+  styleUrl: './index-actors.component.css'
+})
+export class IndexActorsComponent {
+
+}
