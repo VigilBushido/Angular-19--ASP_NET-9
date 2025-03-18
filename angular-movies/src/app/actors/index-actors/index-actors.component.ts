31d5d138c2baa22ec2cbeb03bf0d866a212585e7
VigilBushido <vigilbushido@gmail.com>
2025-03-17 23:40:45 -0400

    +navigation

--- angular-movies/src/app/actors/index-actors/index-actors.component.ts
+++ angular-movies/src/app/actors/index-actors/index-actors.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-index-actors',
-  imports: [],
+  imports: [RouterLink, NzIconModule, NzButtonModule],
   templateUrl: './index-actors.component.html',
   styleUrl: './index-actors.component.css'
 })
