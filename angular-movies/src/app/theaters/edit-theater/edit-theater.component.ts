d15514db1faf7da0b8d4879d6a46f037f7c907dc
VigilBushido <vigilbushido@gmail.com>
2025-03-19 22:21:04 -0400

    + edit components & routes

---
+++ angular-movies/src/app/theaters/edit-theater/edit-theater.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-theater',
+  imports: [],
+  templateUrl: './edit-theater.component.html',
+  styleUrl: './edit-theater.component.css'
+})
+export class EditTheaterComponent {
+
+}
