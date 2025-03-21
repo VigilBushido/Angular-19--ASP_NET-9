76d7d31969db9c564f4bf732e5e5185af2429080
VigilBushido <vigilbushido@gmail.com>
2025-03-20 22:35:12 -0400

    s.c.

--- angular-movies/src/app/theaters/create-theater/create-theater.component.ts
+++ angular-movies/src/app/theaters/create-theater/create-theater.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input, numberAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-create-theater',
@@ -8,4 +8,6 @@ import { Component } from '@angular/core';
 })
 export class CreateTheaterComponent {
 
+  @Input({ transform: numberAttribute })
+  id!: number;
 }
