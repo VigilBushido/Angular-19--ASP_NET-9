e3d70ce1d2a82640e53e55827fe7ce0b99cd9b09
VigilBushido <vigilbushido@gmail.com>
2025-03-23 23:14:24 -0400

    s.c.

--- angular-movies/src/app/genres/create-genre/create-genre.component.ts
+++ angular-movies/src/app/genres/create-genre/create-genre.component.ts
@@ -1,17 +1,23 @@
 import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzIconModule } from 'ng-zorro-antd/icon';
 
 @Component({
   selector: 'app-create-genre',
-  imports: [NzIconModule, NzButtonModule],
+  imports: [NzIconModule, NzButtonModule, ReactiveFormsModule],
   templateUrl: './create-genre.component.html',
   styleUrl: './create-genre.component.css'
 })
 export class CreateGenreComponent {
 
   router = inject(Router);
+  private formBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.group({
+    name: ['']
+  });
 
   saveChanges() {
     //.. save changes
