b5a6a6f8f73faba2e038e384ee22275312a0c66f
VigilBushido <vigilbushido@gmail.com>
2025-04-11 23:26:01 -0400

    init genres form

---
+++ angular-movies/src/app/genres/genres-form/genres-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-genres-form',
+  imports: [],
+  templateUrl: './genres-form.component.html',
+  styleUrl: './genres-form.component.css'
+})
+export class GenresFormComponent {
+
+}
