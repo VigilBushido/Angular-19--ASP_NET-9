878f8cfa19a490a980b0fcd0460da2361c5c44c6
VigilBushido <vigilbushido@gmail.com>
2025-05-14 17:42:47 -0400

    + Controller and Entity (Genre)

---
+++ MoviesAPI/Controllers/GenresController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesAPI.Entities;
+
+namespace MoviesAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class genresController
+    {
+        [HttpGet]
+        public List<Genre> Get()
+        {
+            var repository = new InMemoryRepository();
+            var genres = repository.GetAllGenres();
+            return genres;
+        }
+    }
+}
---
+++ MoviesAPI/Entities/Genre.cs
@@ -0,0 +1,9 @@
+﻿namespace MoviesAPI.Entities
+{
+    public class Genre
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+
+    }
+}
