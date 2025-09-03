cad4ae92d4871172c2a4b7e11a090616d73afd51
VigilBushido <vigilbushido@gmail.com>
2025-09-02 23:15:43 -0400

    + editing Theater component

--- angular-movies/src/app/theaters/edit-theater/edit-theater.component.ts
+++ angular-movies/src/app/theaters/edit-theater/edit-theater.component.ts
@@ -1,8 +1,10 @@
 import { Component, Input, numberAttribute } from '@angular/core';
+import { TheaterDTO, TheaterCreationDTO } from '../theaters.models';
+import { TheatersFormComponent } from "../theaters-form/theaters-form.component";
 
 @Component({
   selector: 'app-edit-theater',
-  imports: [],
+  imports: [TheatersFormComponent],
   templateUrl: './edit-theater.component.html',
   styleUrl: './edit-theater.component.css'
 })
@@ -10,4 +12,10 @@ export class EditTheaterComponent {
 
   @Input({ transform: numberAttribute })
   id!: number;
+
+  model: TheaterDTO = { id: 1, name: 'Cinemark' };
+
+  saveChanges(theater: TheaterCreationDTO) {
+    console.log('editing the theater', theater);
+  }
 }
