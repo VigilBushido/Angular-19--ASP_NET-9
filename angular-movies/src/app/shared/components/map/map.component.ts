1a08a2dc89c28a788768478e23c256212446260f
VigilBushido <vigilbushido@gmail.com>
2025-09-04 23:57:49 -0400

    + map layers

--- angular-movies/src/app/shared/components/map/map.component.ts
+++ angular-movies/src/app/shared/components/map/map.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { latLng, LeafletMouseEvent, tileLayer } from 'leaflet';
+import { icon, latLng, LeafletMouseEvent, marker, Marker, tileLayer } from 'leaflet';
 import { LeafletModule } from '@bluehalo/ngx-leaflet';
 
 @Component({
@@ -10,6 +10,18 @@ import { LeafletModule } from '@bluehalo/ngx-leaflet';
 })
 export class MapComponent {
 
+  markersOptions = {
+    icon: icon({
+      iconUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png',
+      iconSize: [25, 41],
+      iconAnchor: [13, 41],
+      shadowUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png',
+      shadowSize: [41, 41],
+      iconRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png',
+      shadowRetinaUrl: 'https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png'
+    })
+  };
+
   options = {
     layers: [
       tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', { maxZoom: 18, attribution: '...' })
@@ -18,9 +30,15 @@ export class MapComponent {
     center: latLng(26.054519, -80.181130)
   };
 
+  layers: Marker<any[]>[] = [];
+
   handleMapClick(event: LeafletMouseEvent) {
     const latitude = event.latlng.lat;
     const longitude = event.latlng.lng;
     console.log(latitude, longitude);
+
+    this.layers = [];
+    this.layers.push(marker([latitude, longitude], this.markersOptions)
+    );
   }
 }
