013f9742c368f416b517d864d9f33a50dcbee320
VigilBushido <vigilbushido@gmail.com>
2025-09-15 22:18:10 -0400

    + Title impl

--- angular-movies/src/app/movies/movies-search/movies-search.component.ts
+++ angular-movies/src/app/movies/movies-search/movies-search.component.ts
@@ -1,9 +1,12 @@
 import { Component, inject } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { NzColDirective } from "ng-zorro-antd/grid";
+import { NzFormItemComponent, NzFormLabelComponent, NzFormControlComponent } from "ng-zorro-antd/form";
+import { NzInputModule } from "ng-zorro-antd/input";
 
 @Component({
   selector: 'app-movies-search',
-  imports: [],
+  imports: [ReactiveFormsModule, NzColDirective, NzFormItemComponent, NzFormLabelComponent, NzFormControlComponent, NzInputModule],
   templateUrl: './movies-search.component.html',
   styleUrl: './movies-search.component.css'
 })
