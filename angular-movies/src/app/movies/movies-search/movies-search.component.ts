4c2ecd5743fc8877c88366b9934bc10c297ccbea
VigilBushido <vigilbushido@gmail.com>
2025-09-22 22:28:28 -0400

    + movieslist component

--- angular-movies/src/app/movies/movies-search/movies-search.component.ts
+++ angular-movies/src/app/movies/movies-search/movies-search.component.ts
@@ -7,10 +7,11 @@ import { NzSelectModule } from 'ng-zorro-antd/select';
 import { GenreReadDTO } from '../../genres/genres.models';
 import { NzCheckboxModule } from 'ng-zorro-antd/checkbox';
 import { NzButtonComponent } from 'ng-zorro-antd/button';
+import { MoviesListComponent } from '../movies-list/movies-list.component';
 
 @Component({
   selector: 'app-movies-search',
-  imports: [FormsModule, ReactiveFormsModule, NzColDirective, NzFormItemComponent, NzFormLabelComponent, NzFormControlComponent, NzInputModule, NzSelectModule, NzCheckboxModule, NzButtonComponent],
+  imports: [FormsModule, ReactiveFormsModule, NzColDirective, NzFormItemComponent, NzFormLabelComponent, NzFormControlComponent, NzInputModule, NzSelectModule, NzCheckboxModule, NzButtonComponent, MoviesListComponent],
   templateUrl: './movies-search.component.html',
   styleUrl: './movies-search.component.css'
 })
@@ -29,6 +30,64 @@ export class MoviesSearchComponent {
     { id: 1, name: 'Action' },
     { id: 2, name: 'Comedy' },
     { id: 3, name: 'Drama' }];
+
+  moviesOriginal = [{
+    title: 'Inside Out 2',
+    releaseDate: new Date(),
+    price: 1400.99,
+    poster: 'https://upload.wikimedia.org/wikipedia/en/f/f7/Inside_Out_2_poster.jpg?20240514232832',
+    genres: [1, 2, 3],
+    upcomingRelease: true,
+    inTheaters: false
+  },
+  {
+    title: 'Moana 2',
+    releaseDate: new Date('2016-05-03'),
+    price: 300.99,
+    poster: 'https://upload.wikimedia.org/wikipedia/en/7/73/Moana_2_poster.jpg',
+    genres: [3],
+    upcomingRelease: false,
+    inTheaters: true
+  },
+  {
+    title: 'Bad Boys: Ride or Die',
+    releaseDate: new Date('2016-05-03'),
+    price: 300.99,
+    poster: 'https://upload.wikimedia.org/wikipedia/en/8/8b/Bad_Boys_Ride_or_Die_%282024%29_poster.jpg',
+    genres: [1, 3],
+    upcomingRelease: true,
+    inTheaters: false
+  },
+  {
+    title: 'Deadpool & Wolverine',
+    releaseDate: new Date('2016-05-03'),
+    price: 300.99,
+    poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4c/Deadpool_%26_Wolverine_poster.jpg/220px-Deadpool_%26_Wolverine_poster.jpg',
+    genres: [3],
+    upcomingRelease: false,
+    inTheaters: false
+
+  },
+  {
+    title: 'Oppenheimer',
+    releaseDate: new Date('2016-05-03'),
+    price: 300.99,
+    poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/4/4a/Oppenheimer_%28film%29.jpg/220px-Oppenheimer_%28film%29.jpg',
+    genres: [2],
+    upcomingRelease: false,
+    inTheaters: true
+  },
+  {
+    title: 'The Flash',
+    releaseDate: new Date('2016-05-03'),
+    price: 300.99,
+    poster: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/ed/The_Flash_%28film%29_poster.jpg/220px-The_Flash_%28film%29_poster.jpg',
+    genres: [1, 2, 3],
+    upcomingRelease: true,
+    inTheaters: false
+  }];
+
+  movies = this.moviesOriginal;
 
   clear() {
     this.form.patchValue({
