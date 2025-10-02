1b1bf671128aaed6152727fd39b95164ed95fa0e
VigilBushido <vigilbushido@gmail.com>
2025-10-01 21:25:41 -0400

    + Create Movie impl Movie Form

--- angular-movies/src/app/movies/movies-form/movies-form.component.ts
+++ angular-movies/src/app/movies/movies-form/movies-form.component.ts
@@ -1,10 +1,16 @@
-import { Component, EventEmitter, inject, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, inject, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { MovieCreationDTO, MovieDTO } from '../movies.models';
-import { FormBuilder, FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzDatePickerModule } from 'ng-zorro-antd/date-picker';
+import { NzFormModule } from 'ng-zorro-antd/form';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { InputImgComponent } from '../../shared/components/input-img/input-img.component';
 
 @Component({
   selector: 'app-movies-form',
-  imports: [],
+  imports: [NzIconModule, NzButtonModule, ReactiveFormsModule, NzFormModule, NzInputModule, NzDatePickerModule, FormsModule, InputImgComponent],
   templateUrl: './movies-form.component.html',
   styleUrl: './movies-form.component.css'
 })
@@ -15,6 +21,26 @@ export class MoviesFormComponent implements OnInit {
 
   @Output()
   postForm = new EventEmitter<MovieCreationDTO>();
+
+  @ViewChild('datePickerInput') datePickerInput: any;
+
+  closeDatePicker(event: any) {
+    // You can also add logic here to handle the selected date (event)
+    console.log('Selected date:', event);
+    // this.form.controls.dateOfBirth.setValue(event);
+    this.datePickerInput.blur();
+  }
+
+  selectedDate: Date | null = null;
+
+  onDateSelect(event: any): void {
+    // Handle the date change event
+    const date = event as Date | null;
+    this.selectedDate = date;
+    console.log('Selected date:', this.selectedDate);
+    console.log('Form value:', this.form.controls.releaseDate.value);
+    // this.isDatePickerOpen = false;
+  }
 
   private formBuilder = inject(FormBuilder);
   form = this.formBuilder.group({
@@ -28,6 +54,26 @@ export class MoviesFormComponent implements OnInit {
     if (this.model !== undefined) {
       this.form.patchValue(this.model);
     }
+
+    // Listen to form control value changes
+    this.form.controls.releaseDate.valueChanges.subscribe(value => {
+      console.log('Release date changed:', value);
+      this.selectedDate = value;
+    });
+  }
+
+  getErrorMessagesForDateOfBirth(): string {
+    let field = this.form.controls.releaseDate;
+
+    if (field.hasError('required')) {
+      return "The release date field is required";
+    }
+
+    if (field.hasError('dateCannotBeInTheFuture')) {
+      return field.getError('dateCannotBeInTheFuture').message;
+    }
+
+    return "";
   }
 
   handleFileSelection(file: File) {
