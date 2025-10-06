da32b58e5924e6906137c5e055bcaaa9340451ac
VigilBushido <vigilbushido@gmail.com>
2025-10-05 23:57:46 -0400

    + init multiple selector /shared/component

---
+++ angular-movies/src/app/shared/components/multiple-selector/multiple-selector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multiple-selector',
+  imports: [],
+  templateUrl: './multiple-selector.component.html',
+  styleUrl: './multiple-selector.component.css'
+})
+export class MultipleSelectorComponent {
+
+}
