310bea9f498738f94ffe9f586ef143910bbf90fb
VigilBushido <vigilbushido@gmail.com>
2025-10-07 00:02:57 -0400

    + multiple selector class file impl

--- angular-movies/src/app/shared/components/multiple-selector/multiple-selector.component.ts
+++ angular-movies/src/app/shared/components/multiple-selector/multiple-selector.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { MultipleSelectorDTO } from './MultipleSelectorDTO';
 
 @Component({
   selector: 'app-multiple-selector',
@@ -7,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrl: './multiple-selector.component.css'
 })
 export class MultipleSelectorComponent {
+
+  @Input()
+  selectedElements: MultipleSelectorDTO[] = [];
+
+  @Input()
+  nonSelectedElements: MultipleSelectorDTO[] = [];
+
+  select(element: MultipleSelectorDTO, index: number) {
+    this.selectedElements.push(element);
+    this.nonSelectedElements.splice(index, 1);
+  }
+
+  deselect(element: MultipleSelectorDTO, index: number) {
+    this.nonSelectedElements.push(element);
+    this.selectedElements.splice(index, 1);
+  }
+
+  selectAll() {
+    this.selectedElements.push(...this.nonSelectedElements);
+    this.nonSelectedElements = [];
+  }
+
+  deselectAll() {
+    this.nonSelectedElements.push(...this.selectedElements);
+    this.selectedElements = [];
+  }
 
 }
