75fe58c2b74a079009fc25e0fc33f88e9dee7c13
VigilBushido <vigilbushido@gmail.com>
2025-10-15 21:19:32 -0400

    + actors autocomplete

---
+++ angular-movies/src/app/actors/actors-autocomplete/actors-autocomplete.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-actors-autocomplete',
+  imports: [],
+  templateUrl: './actors-autocomplete.component.html',
+  styleUrl: './actors-autocomplete.component.css'
+})
+export class ActorsAutocompleteComponent {
+
+}
--- angular-movies/src/app/movies/movies-form/movies-form.component.ts
+++ angular-movies/src/app/movies/movies-form/movies-form.component.ts
@@ -10,10 +10,11 @@ import { NzInputModule } from 'ng-zorro-antd/input';
 import { InputImgComponent } from '../../shared/components/input-img/input-img.component';
 import { MultipleSelectorDTO } from '../../shared/components/multiple-selector/MultipleSelectorDTO';
 import { MultipleSelectorComponent } from "../../shared/components/multiple-selector/multiple-selector.component";
+import { ActorsAutocompleteComponent } from "../../actors/actors-autocomplete/actors-autocomplete.component";
 
 @Component({
   selector: 'app-movies-form',
-  imports: [NzIconModule, NzButtonModule, ReactiveFormsModule, NzFormModule, NzInputModule, NzDatePickerModule, FormsModule, InputImgComponent, RouterLink, MultipleSelectorComponent],
+  imports: [NzIconModule, NzButtonModule, ReactiveFormsModule, NzFormModule, NzInputModule, NzDatePickerModule, FormsModule, InputImgComponent, RouterLink, MultipleSelectorComponent, ActorsAutocompleteComponent],
   templateUrl: './movies-form.component.html',
   styleUrl: './movies-form.component.css'
 })
